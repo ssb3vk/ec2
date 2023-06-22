0531e39b2bd3fb05d39a06b3e59480d9bf01efcb
ssb3vk <ssb3vk@virgina.edu>
2023-06-21 21:52:49 -0400

    Added better return result on ec2 access

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import AWS from 'aws-sdk'
 
 const app = express()
+app.set('json spaces', 2)
+
+AWS.config.update({region: 'REGION'});
+var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+var params = {
+        DryRun: false
+};
 
 app.listen(5001, () => console.log('Api running on port 5001'))
 
-app.get('/', (req, res) => res.json('My API running'))
+ec2.describeInstances(params, function(err, data) {
+        if (err) {
+                app.get('/', (req, res) => res.json('ERROR', err.stack));
+        } else {
+                app.get('/', (req, res) => res.json(data));
+        }
+});
+
